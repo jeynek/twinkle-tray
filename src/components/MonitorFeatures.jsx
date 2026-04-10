4fa098b9fc2d82e6da0cbb1938d0a788e6d2fd75
jeynek <41385376+jeynek@users.noreply.github.com>
2026-04-10 14:40:18 +0200

    Refactor input data and rendering in MonitorFeatures
    Updated inputsData to simplify input labels and modified rendering logic.

--- src/components/MonitorFeatures.jsx
+++ src/components/MonitorFeatures.jsx
@@ -13,22 +13,7 @@ export default function MonitorFeatures(props) {
     let extraHTML = []
 
     const inputsData = {
-        1: "VGA-1",
-        2: "VGA-2",
-        3: "DVI-1",
-        4: "DVI-2",
-        5: "Composite video 1",
-        6: "Composite video 2",
-        7: "S-Video-1",
-        8: "S-Video-2",
-        9: "Tuner-1",
-        10: "Tuner-2",
-        11: "Tuner-3",
-        12: "Component video (YPrPb/YCrCb) 1",
-        13: "Component video (YPrPb/YCrCb) 2",
-        14: "Component video (YPrPb/YCrCb) 3",
-        15: "DisplayPort-1",
-        16: "DisplayPort-2",
+        15: "DisplayPort",
         17: "HDMI-1",
         18: "HDMI-2"
     }
@@ -92,8 +77,8 @@ export default function MonitorFeatures(props) {
                     <SettingsChild description={
                         <>
                             <div style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
-                                {(Array.isArray(monitor.features["0x60"][1]) ? monitor.features["0x60"][1] : [])?.map(e =>
-                                    <div key={e + monitor.id} className="button" style={{ color: monitor.features[vcp] === e ? "red" : '' }} disabled={monitor.features[vcp] === e}>{inputsData[e]}</div>
+                                { [15, 17, 18].map(e =>
+                                    <div key={e + monitor.id} className="button" style={{ color: monitor.features[vcp] === e ? "red" : '' }} disabled={monitor.features[vcp] === e}>{inputsData[e] || `${T.t("PANEL_LABEL_INPUTS")} (${e})`}</div>
                                 )}
                             </div>
                             <div style={{ marginTop: "10px" }}>
